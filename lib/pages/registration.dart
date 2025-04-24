88d9c493935cebbbf9c2079afcc6aacf9e7e207a
Tahmid163 <tahmidabedin76@gmail.com>
2025-04-24 14:29:06 +0600

    image added

--- lib/pages/registration.dart
+++ lib/pages/registration.dart
@@ -8,95 +8,17 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
-  final _formKey = GlobalKey<FormState>();
-  String gender = 'Male';
-
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController idController = TextEditingController();
-  final TextEditingController ageController = TextEditingController();
-  final TextEditingController addressController = TextEditingController();
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Registration Form"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: 'Name'),//HELLOW
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                controller: idController,
-                decoration: const InputDecoration(labelText: 'ID'),
-                keyboardType: TextInputType.number,
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                controller: ageController,
-                decoration: const InputDecoration(labelText: 'Age'),
-                keyboardType: TextInputType.number,
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                controller: addressController,
-                decoration: const InputDecoration(labelText: 'Address'),
-              ),
-              const SizedBox(height: 12),
-              DropdownButtonFormField<String>(
-                value: gender,
-                items: const [
-                  DropdownMenuItem(value: 'Male', child: Text('Male')),
-                  DropdownMenuItem(value: 'Female', child: Text('Female')),
-                  DropdownMenuItem(value: 'Other', child: Text('Other')),
-                ],
-                onChanged: (value) {
-                  setState(() {
-                    gender = value!;
-                  });
-                },
-                decoration: const InputDecoration(labelText: 'Gender'),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-
-                  showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                      title: const Text('Submitted'),
-                      content: Text(
-                        'Name: ${nameController.text}\n'
-                        'ID: ${idController.text}\n'
-                        'Age: ${ageController.text}\n'
-                        'Address: ${addressController.text}\n'
-                        'Gender: $gender',
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text("OK"),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                ),
-                child: const Text('Submit', style: TextStyle(fontSize: 18)),
-              ),
-            ],
-          ),
-        ),
+      body : Center(
+        child : Image.asset('assets/image1.png')
+      
       ),
     );
   }
+
+
 }
